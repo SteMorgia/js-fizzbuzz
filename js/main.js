8ca81838e72246eafdcd9304b391e7dc5a333936
Stefano Morgia <ste.morgia@gmail.com>
2022-07-06 17:44:44 +0200

    esercizio completato

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 const container = document.getElementById('container');
 
-for (let i = 0; i <= 100; i++) {
-    container.innerHTML += `<div class="box">${i}</div>`;
-}
-if ( i % 3 == 0) {
-    
+for (let i = 1; i <= 100; i++) {
+    let contenuto = i;
+    let classe = "";
+    if ( i % 3 == 0) {
+        contenuto  = "buzz";
+        classe = "buzz";
+    }
+    if ( i % 5 == 0) {
+        contenuto = "fizz";
+        classe = "fizz";
+    }
+    if (i % 15 == 0) {
+        contenuto = "fizzbuzz";
+        classe = "fizzbuzz";
+    }
+
+    container.innerHTML += `<div class="box ${classe}">${contenuto}</div>`;
 }
